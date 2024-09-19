0e49b0197d5aa61698cf06c9bcaefb0e3c3e7230
senpuki1do <senpuki1do@gmail.com>
2024-09-19 02:51:29 +0000

    ww

--- src/docs/Header.tsx
+++ src/docs/Header.tsx
@@ -1,6 +1,31 @@
 function Header() {
     return (
-        <h1>This is header</h1>
+        <header>
+            <div className="title">
+                <h1>Sonnekoのサイト</h1>
+                <nav className="navigation">
+                    <ul>
+                        <li><a className="listItem" href="/">Home</a></li>
+                        <li><a className="listItem" href="/intro">Intro</a></li>
+                        <li><a className="listItem" href="/links">Links</a></li>
+                        <li><a className="listItem" href="/blog">Blog</a></li>
+                        <li><a className="listItem" href="/game">Game</a></li>
+                    </ul>
+                </nav>
+                <div className="hamburgerBtn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="hamburgerMenu">
+                    <li><a className="listItem" href="/">Home</a></li>
+                    <li><a className="listItem" href="/intro">Intro</a></li>
+                    <li><a className="listItem" href="/links">Links</a></li>
+                    <li><a className="listItem" href="/blog">Blog</a></li>
+                    <li><a className="listItem" href="/game">Game</a></li>
+                </div>
+            </div>
+        </header>
     )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,30 @@
+// import my css
+import "./css/Home.css";
+
+function Card({ title, content }: { title: string, content: string }) {
+    return (
+        <>
+            <h2>{title}</h2>
+            <p>{content}</p>
+        </>
+    )
+}
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <div className="home">
+                <div className="title">
+                    <h1>Sonnekoのサイト</h1>
+                    <p>Sonnekoが、個人で、遊び半分で作って、完全無料で、運営しているサイトです。</p>
+                </div>
+                <div className="contents">
+                    <Card title="Sonnekoとは誰？" content="sonnekoは、プログラミング・物理・数学・生物などに興味を持っている高校生です。" />
+                    <Card title="何のためにサイトを作ったの？" content="ひとえに、練習のためです。Reactの練習をしたかったのです。また、作ったものを公開する場所としての役割もあります。" />
+                    <Card title="最近何してる？" content="c++にハマり中です。Pythonとかでバックエンドもやりたい。あと、物理やりたい" />
+                </div>
+            </div>
+        </>
     )
 }
 
