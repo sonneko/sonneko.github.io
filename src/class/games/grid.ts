0c1f436a208f0e978efb79b05cced5261e5cbf95
senpuki1do <senpuki1do@gmail.com>
2024-11-01 15:25:24 +0000

    l

--- src/class/games/grid.ts
+++ src/class/games/grid.ts
@@ -1,33 +1,33 @@
-import {Pos, Angle} from './common'
+// import {Pos, Angle} from './common'
 
-class Item {
-    positionAbs: Pos
-    color: string
-    constructor() {
-        this.positionAbs = new Pos();
-        this.color = 'red';
-    }
-    setColor(colorStr: string) {
-        this.color = colorStr;
-    }
-}
+// class Item {
+//     positionAbs: Pos
+//     color: string
+//     constructor() {
+//         this.positionAbs = new Pos();
+//         this.color = 'red';
+//     }
+//     setColor(colorStr: string) {
+//         this.color = colorStr;
+//     }
+// }
 
-class Canvas {
-    nowPos: Pos
-    items: Item[][]
-    constructor(widthSize: number, heightSize: number) {
-        this.nowPos = new Pos();
-        this.items = [];
-        for (let i = 0; i <= widthSize; i++) {
-            this.items.push([]);
-            for (let j = 0; j <= heightSize; j++) {
-                this.items[i].push(new Item)
-            }
-        }
-        console.log(this.items)
-    }
-}
+// class Canvas {
+//     nowPos: Pos
+//     items: Item[][]
+//     constructor(widthSize: number, heightSize: number) {
+//         this.nowPos = new Pos();
+//         this.items = [];
+//         for (let i = 0; i <= widthSize; i++) {
+//             this.items.push([]);
+//             for (let j = 0; j <= heightSize; j++) {
+//                 this.items[i].push(new Item)
+//             }
+//         }
+//         console.log(this.items)
+//     }
+// }
 
-export {
-    Canvas
-}
+// export {
+//     Canvas
+// }
--- src/pages/game/SF2024Game.tsx
+++ src/pages/game/SF2024Game.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from "react";
-import { Canvas } from "../../class/games/grid";
+// import { useEffect, useState } from "react";
+// import { Canvas } from "../../class/games/grid";
 
 
-function SF2024Game() {
-    const [myCanvas, setMyCanvas] = useState(new Canvas(3,3));
+// function SF2024Game() {
+//     const [myCanvas, setMyCanvas] = useState(new Canvas(3,3));
 
-    useEffect(() => {
-        // ここに処理
-        console.log(myCanvas.items)
-    },[])
-    return (
-        <>
-            <table>
-                {myCanvas.items.map((items, index) => {
-                    return (
-                        <tr>
-                            <td>
-                                {myCanvas.items[index].map((item) => {
-                                    return (
-                                        <div style={{
-                                            height: "100px",
-                                            width: "100px",
-                                            backgroundColor: item.color
-                                       }}></div>
-                                    )
-                                })}
-                            </td>
-                        </tr>
-                    )
-                })}
-            </table>
-        </>
-    )
-}
+//     useEffect(() => {
+//         // ここに処理
+//         console.log(myCanvas.items)
+//     },[])
+//     return (
+//         <>
+//             <table>
+//                 {myCanvas.items.map((items, index) => {
+//                     return (
+//                         <tr>
+//                             <td>
+//                                 {myCanvas.items[index].map((items) => {
+//                                     return (
+//                                         <div style={{
+//                                             height: "100px",
+//                                             width: "100px",
+//                                             backgroundColor: item.color
+//                                        }}></div>
+//                                     )
+//                                 })}
+//                             </td>
+//                         </tr>
+//                     )
+//                 })}
+//             </table>
+//         </>
+//     )
+// }
 
-export default SF2024Game;
+// export default SF2024Game;
